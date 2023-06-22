b9fe3eb16f39177b6950b8a304af8fd96d479665
EvgeniyPosohin <mad.house@bk.ru>
2023-06-22 10:47:37 +0500

    init

---
+++ iter.py
@@ -0,0 +1,60 @@
+class FlatIterator:
+
+    def __init__(self, *args):
+        self.suppl_list = []
+        self.set_plain(args)
+        self.counter = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if self.counter >= len(self.suppl_list):
+            raise StopIteration
+        return self.suppl_list[self.counter]
+
+    def set_plain(self, list_of_list):
+        for item in list_of_list:
+            if type(item) is list:
+                self.set_plain(item)
+            else:
+                self.suppl_list.append(item)
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_1()
+    test_3()
